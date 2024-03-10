7251e5abf15ca8cc09972dfe05a1b2c0e8b1715e
yadeorsl <yadeorsl@gmail.com>
2024-03-10 15:35:11 +0300

    Add allEmployee and getEmployeeByID methods

--- src/main/java/com/lab/employeedemo/controller/EmployeeController.java
+++ src/main/java/com/lab/employeedemo/controller/EmployeeController.java
@@ -3,10 +3,9 @@ package com.lab.employeedemo.controller;
 import com.lab.employeedemo.model.Employee;
 import com.lab.employeedemo.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +15,13 @@ public class EmployeeController {
    @PutMapping
   public Employee addEmployee(@RequestBody Employee employee){
      return employeeService.saveEmployee(employee);
+   }
+   @GetMapping
+    public List<Employee> allEmployee(){
+       return employeeService.allEmployee();
+   }
+   @GetMapping("{id}")
+    public Employee getEmployeeByID(@PathVariable("id") long id){
+       return employeeService.getEmployeeByID(id);
    }
 }
--- src/main/java/com/lab/employeedemo/service/EmployeeService.java
+++ src/main/java/com/lab/employeedemo/service/EmployeeService.java
@@ -3,10 +3,15 @@ package com.lab.employeedemo.service;
 import com.lab.employeedemo.model.Employee;
 import com.lab.employeedemo.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
@@ -14,4 +19,17 @@ public class EmployeeService {
         return employeeRepository.save(employee);
     }
 
+    public List<Employee> allEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeByID(long id) {
+        Optional<Employee> employee=employeeRepository.findById(id);
+        if (employee.isPresent()){
+            return employee.get();
+        }else {
+            log.info("There is no employee in the id you are looking for");
+            return null;
+        }
+    }
 }
